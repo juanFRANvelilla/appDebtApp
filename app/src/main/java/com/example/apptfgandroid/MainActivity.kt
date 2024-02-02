57b14cef0a32f2c55fb2ba4ba7ac712523787515
juanfran perez <juanfrancisco.perez@grupocarreras.com>
2024-02-02 14:48:15 +0100

    agregar configuracion para color de background

--- app/src/main/java/com/example/apptfgandroid/MainActivity.kt
+++ app/src/main/java/com/example/apptfgandroid/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.apptfgandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.apptfgandroid.navigation.AppNavigation
 import com.example.apptfgandroid.ui.theme.AppTfgAndroidTheme
@@ -18,7 +20,10 @@ class MainActivity : ComponentActivity() {
             AppTfgAndroidTheme(
                 darkTheme = false
             ) {
-                AppNavigation()
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    AppNavigation()
+                }
+
             }
         }
     }
