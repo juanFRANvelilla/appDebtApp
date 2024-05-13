0c902f15f6408eaafd6b3d72489ce0646ca7f00e
juanfran perez <juanfran01velilla@gmail.com>
2024-05-13 13:56:10 +0200

    separar selectable box

--- app/src/main/java/com/example/apptfgandroid/ui/screens/currentDebts/CurrentDebtsView.kt
+++ app/src/main/java/com/example/apptfgandroid/ui/screens/currentDebts/CurrentDebtsView.kt
@@ -53,6 +53,8 @@ import androidx.navigation.NavHostController
 import com.example.apptfgandroid.navigation.AppScreens
 import com.example.apptfgandroid.ui.common.composables.BottomBar
 import com.example.apptfgandroid.ui.common.composables.DebtCard
+import com.example.apptfgandroid.ui.common.composables.PaymentOption
+import com.example.apptfgandroid.ui.common.composables.SelectableBox
 import com.example.apptfgandroid.ui.screens.mainMenu.MainMenuContent
 import com.example.apptfgandroid.ui.screens.mainMenu.TopBar
 import java.time.LocalDate
@@ -94,103 +96,11 @@ fun CurrentDebtsView(navController: NavHostController?) {
         content = { CurrentDebtsContent() },
         bottomBar = { BottomBar(navController) }
     )
-
-
-
-
-
-
 }
 
-enum class PaymentOption {
-    Owe,
-    Owed,
-    Both
-}
-
-@Composable
-fun SelectableBox(
-    selectedOption: PaymentOption,
-    onSelectedOptionChange: (PaymentOption) -> Unit
-){
-    val color1 = if(selectedOption == PaymentOption.Owe) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
-    val color2 = if(selectedOption == PaymentOption.Owe) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.primary
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(40.dp)
-    ) {
-
-        val shapeLeft = GenericShape { size: Size, layoutDirection: LayoutDirection ->
-            val width = size.width / 2
-            val height = size.height
-            moveTo(0f, height)
-            lineTo(0f, 0f)
-            lineTo(width, 0f)
-            lineTo(width, height)
-            close()
-        }
-
-        val shapeRight = GenericShape { size: Size, layoutDirection: LayoutDirection ->
-            val width = size.width / 2
-            val height = size.height
-            val diagonalLength = (width * 0.1f) // Longitud de la diagonal (hipotenusa) para formar un ángulo de 45 grados
-
-            moveTo(width, 0f)
-            lineTo(width - diagonalLength, height) // Punto final de la diagonal para que coincida con la parte izquierda
-            lineTo(size.width, height)
-            lineTo(size.width, 0f)
-            close()
-        }
-
-        val modifierLeft = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .graphicsLayer {
-                clip = true
-                shape = shapeLeft
-            }
 
 
-        val modifierRight = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .graphicsLayer {
-                clip = true
-                shape = shapeRight
-            }
-            .background(Color.Red)
 
-        Box(
-            modifier = modifierLeft
-                .clickable { onSelectedOptionChange(PaymentOption.Owe) }
-                .background(color1),
-        ){
-            Text(
-                modifier = Modifier.padding(start = 10.dp),
-                text = "Deudas",
-                color = color2,
-                fontSize = 18.sp
-            )
-        }
-
-        Box(
-            modifier = modifierRight
-                .clickable { onSelectedOptionChange(PaymentOption.Owed) }
-                .background(color2),
-        ){
-            Text(
-                modifier = Modifier.align(Alignment.BottomEnd).padding(end=10.dp),
-                text = "Cobros",
-                fontSize = 18.sp,
-                color = color1
-            )
-        }
-
-    }
-
-}
 
 
 @Composable
@@ -199,11 +109,10 @@ fun CurrentDebtsContent(){
 
     Column(){
         SelectableBox(
-            selectedOption,
-            {
-                selectedOption = it
-            }
-        )
+            selectedOption
+        ) {
+            selectedOption = it
+        }
 
         val userDTO = UserDTO(
             username = "+346354532",
